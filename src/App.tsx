aa651026c680275e974684407dd65810f6fc29f0
The-Oracle-AT <96382250+The-Oracle-AT@users.noreply.github.com>
2023-09-02 13:05:38 +0200

    Adding Navbar. fix functionality so it still appears after being opened in smaller window then stretched. The problem is thar the tabs disappear unless you close the drop down before expanding the window

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './NavBar.tsx'
+import NavBar from './NavBar.tsx'
+
+function App() {
+  return (
+    <>
+    <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/NavBar.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './NavBar.css';
+
+/*NavBar - a fucntion that creates a navigation bar for a 
+* website. This will be converted to a .module.tsx file for 
+* reusability in differents site.
+* notice that the navbar can either be navbarcollpased or just navbar
+* upon toggling a prefix is added. The toggle is the screen size which
+* is specified in the CSS style sheet
+* param : No params
+* 
+* Return: A navigation bar that will be rendered on a webpage*/
+const NavBar = () => {
+    const [collapsed, setCollapsed] = useState(false);
+     
+    /* toggleCollapsed - to toggle from callapsed and 
+    * !collapsed nav */
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed); 
+     };
+     
+     return (
+        <nav id= "navbar" className = {`navbar ${collapsed ? 'collapsed' : ''}`}>
+            <div className='navbar-brand'>Logo</div>
+            <div className={`navbar-links ${ collapsed ? 'collapsed' : ''}`}>
+                <a href='#'>Home</a>
+                <a href='#'>Services</a>
+                <a href='#'>About</a>
+                <a href='#'>Contact</a>
+            </div>
+            
+            {/* This is the toggle icon*/}
+            <div className='navbar-toggle' onClick={ toggleCollapsed }>
+                <div className = {`icon ${ collapsed ? 'open' : ''}`}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </nav>
+     );
+};
+const HomeContent = () =>{
+    return(
+        <div className='home-content'>
+            
+        </div>
+    );
+}
+
+
+
+export default NavBar;
